3a251e5883ae14f68f96b075732e80c228786991
tianyunchong <854024109a>
2019-10-24 16:25:59 +0800

    初始化translator

---
+++ src/DependencyInjection/TranslatorExtension.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * User: tianyunchong
+ * Date: 2019/10/24
+ * Time: 15:51
+ */
+namespace Van\Translation\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\Config\FileLocator;
+use Van\Redis\Constant;
+
+
+class RedisExtension extends Extension
+{
+
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader($container, new FileLocator(\dirname(__DIR__) . '/Resources/config'));
+        $loader->load('services.yaml');
+
+        $configuration = $this->getConfiguration($configs, $container);
+        $config = $this->processConfiguration($configuration, $configs);
+        
+        $container->setParameter(Constant::REDIS_CONFIG, $config);
+    }
+
+    public function getConfiguration(array $config, ContainerBuilder $container)
+    {
+        return new Configuration($container->getParameter('kernel.debug'));
+    }
+
+}
